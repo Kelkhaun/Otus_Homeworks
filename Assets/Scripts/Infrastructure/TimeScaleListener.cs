cea0812500cb89cf2fdb16771a83fa0f6303f1bc
Ilya Shilov <80789057+Kelkhaun@users.noreply.github.com>
2023-11-18 13:43:37 +0300

    Update TimeScaleListener.cs

--- Assets/Scripts/Infrastructure/TimeScaleListener.cs
+++ Assets/Scripts/Infrastructure/TimeScaleListener.cs
@@ -2,16 +2,34 @@
 
 namespace Infrastructure
 {
-    public class TimeScaleListener : MonoBehaviour, IGamePauseListener, IGameResumeListener
+    public class TimeScaleListener : MonoBehaviour, IGamePauseListener, IGameResumeListener, IGameFinishListener, IGameStartListener
     {
+        private int _maxTimeScale = 1;
+        private int _minTimeScale = 0;
+
         public void OnResumeGame()
         {
-            Time.timeScale = 1;
+            SetTimeScale(_maxTimeScale);
         }
 
         public void OnPauseGame()
         {
-            Time.timeScale = 0;
+            SetTimeScale(_minTimeScale);
+        }
+
+        public void OnFinishGame()
+        {
+            SetTimeScale(_minTimeScale);
+        }
+
+        public void OnStartGame()
+        {
+            SetTimeScale(_maxTimeScale);
+        }
+
+        private void SetTimeScale(float timeScale)
+        {
+            Time.timeScale = timeScale;
         }
     }
 }
